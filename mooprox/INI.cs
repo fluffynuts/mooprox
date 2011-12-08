96e2bf09cd2a1355347537d4a4e406ae1141251b
davydm@gmail.com <davydm@gmail.com@0d8433d8-5c0f-c378-7c7d-5eff5b645acc>
2011-12-08 10:43:39 +0000

    * initial checkin

---
+++ mooprox/INI.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SimpleINI
+{
+    class INI
+    {
+        public Dictionary<string, Dictionary<string, string>> Config;
+        public bool Loaded { get { return this._loaded; } }
+        private bool _loaded;
+        public INI()
+        {
+            this._loaded = false;
+            this.Config = new Dictionary<string,Dictionary<string,string>>();
+        }
+        public INI(string loadFilePath)
+        {
+            this.Config = new Dictionary<string,Dictionary<string,string>>();
+            this._loaded = false;
+            this.LoadFile(loadFilePath);
+        }
+
+        public INI SetValue(string section, string name, string value)
+        {
+            if (!this.Config.ContainsKey(section))
+                this.Config[section] = new Dictionary<string, string>();
+            this.Config[section][name] = value;
+            return this;
+        }
+
+        public bool LoadFile(string path)
+        {
+            if (!File.Exists(path))
+                return false;
+            FileStream fs;
+            try
+            {
+                fs = File.Open(path, FileMode.Open);
+            }
+            catch
+            {
+                return false;
+            }
+            StreamReader rdr = new StreamReader(fs);
+
+            // reused splitters
+            char[] commentDelimiter = new char[] {';'};
+            char[] itemDelimiter = new char[] {'='};
+            char[] sectionTrim = new char[] {'[', ']', ' ', '\t', '\n', '\r'};
+
+            string currentSection = "";
+            while (rdr.Peek() > 0)
+            {
+                string line = rdr.ReadLine().Trim();
+                if (line.Length == 0)   // ignore an empty line
+                    continue;
+                // split out comments
+                string[] parts = line.Split(commentDelimiter);
+                if (parts[0].Length == 0)   // ignore a comment-only line
+                    continue;
+                if (parts[0][0] == '[') // start of a section
+                {
+                    currentSection = parts[0].Trim(sectionTrim);
+                    continue;
+                }
+                parts = parts[0].Split(itemDelimiter);
+                if (parts.Length < 2)
+                    continue;
+                string name = parts[0].Trim();
+
+                if (!this.Config.ContainsKey(currentSection))
+                    this.Config[currentSection] = new Dictionary<string, string>();
+                if (this.Config[currentSection].ContainsKey(name))
+                    continue;   // already specified higher up
+
+                List<string> tmp = new List<string>();
+                for (var i = 1; i < parts.Length; i++)
+                    tmp.Add(parts[i]);
+                this.Config[currentSection][name] = String.Join("=", tmp.ToArray()).Trim();
+            }
+
+            rdr.Close();
+            fs.Close();
+
+            this._loaded = true;
+            return this._loaded;
+        }
+
+        public INI DelSection(string section)
+        {
+            if (this.Config.ContainsKey(section))
+                this.Config.Remove(section);
+            return this;
+        }
+        public INI DelSetting(string section, string setting)
+        {
+            if (this.Config.ContainsKey(section))
+            {
+                if (this.Config[section].ContainsKey(setting))
+                {
+                    this.Config[section].Remove(setting);
+                }
+            }
+            return this;
+        }
+
+        public bool WriteFile(string path)
+        {
+            FileStream fs;
+            try
+            {
+                fs = File.Open(path, FileMode.Create);
+            }
+            catch
+            {
+                return false;
+            }
+            StreamWriter wtr = new StreamWriter(fs);
+            foreach (var s in this.Config.Keys)
+            {
+                wtr.WriteLine(String.Format("[{0}]", s));
+                foreach (var n in this.Config[s].Keys)
+                {
+                    wtr.WriteLine(String.Format("{0}={1}", n, this.Config[s][n]));
+                }
+                wtr.WriteLine("");
+            }
+
+            wtr.Close();
+            fs.Close();
+            return true;
+        }
+    }
+}
---
+++ mooprox/frmMain.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace mooprox
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lvProxies = new System.Windows.Forms.ListView();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnNoProxy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvProxies
+            // 
+            this.lvProxies.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvProxies.FullRowSelect = true;
+            this.lvProxies.Location = new System.Drawing.Point(2, 4);
+            this.lvProxies.MultiSelect = false;
+            this.lvProxies.Name = "lvProxies";
+            this.lvProxies.Size = new System.Drawing.Size(387, 85);
+            this.lvProxies.TabIndex = 0;
+            this.lvProxies.UseCompatibleStateImageBehavior = false;
+            this.lvProxies.SelectedIndexChanged += new System.EventHandler(this.lvProxies_SelectedIndexChanged);
+            this.lvProxies.DoubleClick += new System.EventHandler(this.lvProxies_DoubleClick);
+            // 
+            // lblHost
+            // 
+            this.lblHost.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(15, 121);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(29, 13);
+            this.lblHost.TabIndex = 1;
+            this.lblHost.Text = "Host";
+            // 
+            // lblPort
+            // 
+            this.lblPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(15, 146);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 2;
+            this.lblPort.Text = "Port";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtHost.Location = new System.Drawing.Point(57, 121);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(133, 20);
+            this.txtHost.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtPort.Location = new System.Drawing.Point(57, 146);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(133, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Location = new System.Drawing.Point(314, 90);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add &New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDel.Location = new System.Drawing.Point(314, 116);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 5;
+            this.btnDel.Text = "&Remove";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSave.Location = new System.Drawing.Point(196, 144);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "&Save proxy";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(16, 96);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtName.Location = new System.Drawing.Point(57, 96);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(133, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(314, 142);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(197, 121);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(74, 23);
+            this.btnApply.TabIndex = 9;
+            this.btnApply.Text = "Use proxy";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // btnNoProxy
+            // 
+            this.btnNoProxy.Location = new System.Drawing.Point(196, 96);
+            this.btnNoProxy.Name = "btnNoProxy";
+            this.btnNoProxy.Size = new System.Drawing.Size(75, 23);
+            this.btnNoProxy.TabIndex = 10;
+            this.btnNoProxy.Text = "No proxy";
+            this.btnNoProxy.UseVisualStyleBackColor = true;
+            this.btnNoProxy.Click += new System.EventHandler(this.btnNoProxy_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 170);
+            this.Controls.Add(this.btnNoProxy);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblHost);
+            this.Controls.Add(this.lvProxies);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "frmMain";
+            this.Text = "XproX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvProxies;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnNoProxy;
+    }
+}
+
